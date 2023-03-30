92068b309034822ce3ff48e7dbe6a5b12ac5374d
Emil Christensen <emil@prefect.io>
2023-03-30 12:59:04 -0400

    Run deployment from a task for async execution

--- flows.py
+++ flows.py
@@ -1,6 +1,6 @@
 import subprocess
 
-from prefect import flow, get_run_logger
+from prefect import flow, task, get_run_logger
 from prefect.deployments import run_deployment
 
 
@@ -13,18 +13,27 @@ def version():
         logger.info(line)
 
 
+@task
+def run_deployment_task(deployment_name):
+    """Run the deployment in a task for async execution"""
+    run_deployment(
+        name=f"version/{deployment_name}",
+    )
+
+
 @flow
 def parent():
+    """Run three deployments in different docker containers"""
     deployment_names = [
         "main-2-8-5-python3-10",
         "main-2-8-6-python3-10",
         "main-2-8-7-python3-10",
     ]
     for deployment_name in deployment_names:
-        run_deployment(
-            name=f"version/{deployment_name}",
+        run_deployment_task.submit(
+            deployment_name,
         )
 
 
 if __name__ == "__main__":
-    version()
+    parent()
